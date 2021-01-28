503fe71f81426fca6a433eeeaecbb2a4c5704695
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-01-28 13:29:17 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import "./App.scss";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.scss";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="jumbotron">
+        <div className="main-wraper">
+          <img src="" alt="" />
+          <div className="jumbotron-content">dkfjsdkljf</div>
+        </div>
+      </div>
+      <div className="main-wraper"></div>
+    </div>
+  );
+}
