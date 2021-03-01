ac988a486417c4445d3a5db9d81d1b2433a62122
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-03-01 20:54:22 +0500

    commit

--- src/screens/Testimonails/Testimonails.js
+++ src/screens/Testimonails/Testimonails.js
@@ -8,7 +8,136 @@ export default function Testimonails() {
         <div className="section-heading">TESTIMONIALS</div>
         <div className="section-about">What People Says</div>
       </div>
-      <div className="section-content"></div>
+      <div className="section-content">
+        <div className="testimonals-slider">
+          <div className="testimonals-slider-left">
+            <button className="testimonals-slider-btn">i</button>
+          </div>
+          <div className="testimonals-slider-center">
+            <div className="testimonals-card">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 57 237"
+                fill="currentColor"
+              >
+                <text
+                  id="_"
+                  data-name='"'
+                  transform="translate(57 45) rotate(180)"
+                  font-size="200"
+                  font-family="PerpetuaTitlingMT-Light, Perpetua Titling MT"
+                  font-weight="300"
+                >
+                  <tspan x="0" y="0">
+                    &quot;
+                  </tspan>
+                </text>
+              </svg>
+              <p className="testimonals-card-details">
+                Lorem ipsum dolor sit amet,Stet clita kasd gubergren, no sea
+                takimata sanctus est is the lorem ipsum dolor sit amet. sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore is the magna
+                aliquyam.
+              </p>
+              <div className="testimonals-card-name">John Doe</div>
+              <div className="testimonals-card-designation">
+                CEO, ABC Company
+              </div>
+            </div>
+            <div className="testimonals-card">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 57 237"
+                fill="currentColor"
+              >
+                <text
+                  id="_"
+                  data-name='"'
+                  transform="translate(57 45) rotate(180)"
+                  font-size="200"
+                  font-family="PerpetuaTitlingMT-Light, Perpetua Titling MT"
+                  font-weight="300"
+                >
+                  <tspan x="0" y="0">
+                    &quot;
+                  </tspan>
+                </text>
+              </svg>
+              <p className="testimonals-card-details">
+                Lorem ipsum dolor sit amet,Stet clita kasd gubergren, no sea
+                takimata sanctus est is the lorem ipsum dolor sit amet. sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore is the magna
+                aliquyam.
+              </p>
+              <div className="testimonals-card-name">John Doe</div>
+              <div className="testimonals-card-designation">
+                CEO, ABC Company
+              </div>
+            </div>
+            <div className="testimonals-card">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 57 237"
+                fill="currentColor"
+              >
+                <text
+                  id="_"
+                  data-name='"'
+                  transform="translate(57 45) rotate(180)"
+                  font-size="200"
+                  font-family="PerpetuaTitlingMT-Light, Perpetua Titling MT"
+                  font-weight="300"
+                >
+                  <tspan x="0" y="0">
+                    &quot;
+                  </tspan>
+                </text>
+              </svg>
+              <p className="testimonals-card-details">
+                Lorem ipsum dolor sit amet,Stet clita kasd gubergren, no sea
+                takimata sanctus est is the lorem ipsum dolor sit amet. sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore is the magna
+                aliquyam.
+              </p>
+              <div className="testimonals-card-name">John Doe</div>
+              <p className="testimonals-card-designation">CEO, ABC Company</p>
+            </div>
+            <div className="testimonals-card">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 57 237"
+                fill="currentColor"
+              >
+                <text
+                  id="_"
+                  data-name='"'
+                  transform="translate(57 45) rotate(180)"
+                  font-size="200"
+                  font-family="PerpetuaTitlingMT-Light, Perpetua Titling MT"
+                  font-weight="300"
+                >
+                  <tspan x="0" y="0">
+                    &quot;
+                  </tspan>
+                </text>
+              </svg>
+              <p className="testimonals-card-details">
+                Lorem ipsum dolor sit amet,Stet clita kasd gubergren, no sea
+                takimata sanctus est is the lorem ipsum dolor sit amet. sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore is the magna
+                aliquyam.
+              </p>
+              <div className="testimonals-card-name">John Doe</div>
+              <div className="testimonals-card-designation">
+                CEO, ABC Company
+              </div>
+            </div>
+          </div>
+          <div className="testimonals-slider-right">
+            <button className="testimonals-slider-btn">i</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
