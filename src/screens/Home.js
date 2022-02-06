1af3534a178a93fb6d717c52daa60cd20b42cd4a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-02-06 16:37:21 +0500

    portfolio section done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Feather } from "react-feather";
+import { ArrowRight, Feather } from "react-feather";
+import { Link } from "react-router-dom";
+import { PortfolioFilter, ProjectCard } from "../components";
 
 function ServicesCard({ name, icon }) {
   return (
@@ -50,8 +52,66 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="about__section" id="about__section"></section>
-      <section className="portfolio__section" id="portfolio__section"></section>
+      <section className="about__section" id="about__section">
+        <div className="about__section__left"></div>
+        <div className="about__section__right"></div>
+      </section>
+      <div className="portfolio__section__header">
+        <div className="portfolio__section__left">
+          <div className="services__section__right__sub__heading">
+            <div className="services__section__right__sub__heading__bar"></div>
+            Portfolio
+          </div>
+          <div
+            className="services__section__right__heading"
+            style={{ marginBottom: 0 }}
+          >
+            Some of my distinguished woks
+          </div>
+        </div>
+        <div className="portfolio__section__right">
+          <div className="portfolio__filters">
+            <PortfolioFilter defaultChecked={true}>All</PortfolioFilter>
+            <PortfolioFilter>Web App</PortfolioFilter>
+            <PortfolioFilter>Mobile App</PortfolioFilter>
+            <PortfolioFilter>UI/UX Design</PortfolioFilter>
+          </div>
+        </div>
+      </div>
+      <div className="portfolio__projects">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+      <div
+        className="project__section__left__link"
+        style={{
+          width: "100%",
+          maxWidth: "1440px",
+          margin: "0em auto",
+          padding: "0em 2em",
+          marginTop: "1em",
+          marginBottom: "6em",
+        }}
+      >
+        <Link
+          to="/portfolio"
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
+          className="project__section__left__link__entry"
+          style={{ marginTop: "0em" }}
+        >
+          View More
+          <ArrowRight size={20} color="currentColor" />
+        </Link>
+      </div>
       <section
         className="testimonals__section"
         id="testimonals__section"
