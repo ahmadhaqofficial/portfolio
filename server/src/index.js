9326b4277fef0744c217ca36afbc8997317aef0a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-02-17 22:36:13 +0500

    base api done

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,9 @@ const cloudinary = require("cloudinary");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
+const contactRouter = require("./routers/contactRouter");
+const projectRouter = require("./routers/projectRouter");
 
 // api config
 dotenv.config();
@@ -12,11 +15,21 @@ const port = process.env.PORT || 9000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload());
 
 // db config
-mongoose.connect(process.env.MONGO_DB_URL, () => {
-  console.log("db connected");
-});
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database");
+  })
+  .catch((error) => {
+    console.log("database connection failed. exiting now...");
+    console.error(error);
+  });
 cloudinary.config({
   cloud_name: "mehfoozurrehman",
   api_key: "915662453295273",
@@ -26,17 +39,21 @@ cloudinary.config({
 // api endpoints
 app.get("/", (req, res) => {
   res.json({
-    message: "hey",
+    message: "hey welcome to mehfooz-ur-rehman's portfolio",
   });
 });
-app.post("/upload", (req, res) => {
+app.post("/upload", function (req, res) {
+  const image = req.files.image;
+  console.log(image);
   cloudinary.v2.uploader.upload(
     "https://avatars.githubusercontent.com/u/69617285?v=4",
-    { public_id: "bilal" },
+    { public_id: "asad" },
     function (error, result) {
       console.log(result);
     }
   );
 });
+app.use("/api/v1", contactRouter);
+app.use("/api/v1", projectRouter);
 // listners
 app.listen(port, () => console.log(`api working on ${port}`));
--- server/src/models/projectModel.js
+++ server/src/models/projectModel.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema(
+  {
+    name: String,
+    languages: String,
+    image: String,
+    description: String,
+    category: String,
+    date: String,
+    client: String,
+    url: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("projects", projectSchema);
