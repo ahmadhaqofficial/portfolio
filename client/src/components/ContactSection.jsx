2ff52961d799a98fcb164a8f026597054ec26378
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-02-27 23:04:02 +0500

    contact post integration done

--- client/src/components/ContactSection.jsx
+++ client/src/components/ContactSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
+  CheckCircle,
   Facebook,
   GitHub,
   Instagram,
@@ -10,91 +12,155 @@ import {
 } from "react-feather";
 
 export default function ContactSection() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setSuccess(false);
+    }, 1000);
+  }, [success]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:9000/api/v1/set_contact", {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+      })
+      .then(() => {
+        setName("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
+        setSuccess(true);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      })
+      .catch((err) => {
+        console.log("contact error:" + err);
+      });
+  }
   return (
-    <div className="contact__section">
-      <div className="contact__section__left">
-        <div className="contact__section__left__sub__heading">
-          <div className="contact__section__left__sub__heading__bar"></div>
-          Contact Me
-        </div>
-        <div className="contact__section__left__heading">Contact With Me</div>
-        <div className="contact__section__left__info">
-          Let's build you simple, beautiful and intuitive solutions
-        </div>
-        <div className="contact__section__left__link">
-          <div className="contact__section__left__link__icon">
-            <MapPin size={20} color="currentColor" />
+    <>
+      {success ? (
+        <div className="screen__popup">
+          <div className="screen__popup__content">
+            <CheckCircle size={50} color="currentColor" />
+            <div className="screen__popup__content__heading">
+              Successfully Sent
+            </div>
           </div>
-          <span>Samundri - Faisalabad - Pakistan</span>
         </div>
-        <div className="contact__section__left__link">
-          <div className="contact__section__left__link__icon">
-            <Mail size={20} color="currentColor" />
+      ) : null}
+      <div className="contact__section">
+        <div className="contact__section__left">
+          <div className="contact__section__left__sub__heading">
+            <div className="contact__section__left__sub__heading__bar"></div>
+            Contact Me
           </div>
-          <span>mehfoozijaz786@gmail.com</span>
-        </div>
-        <div className="contact__section__left__link">
-          <div className="contact__section__left__link__icon">
-            <Phone size={20} color="currentColor" />
+          <div className="contact__section__left__heading">Contact With Me</div>
+          <div className="contact__section__left__info">
+            Let's build you simple, beautiful and intuitive solutions
           </div>
-          <span>+92 313 7178074</span>
+          <div className="contact__section__left__link">
+            <div className="contact__section__left__link__icon">
+              <MapPin size={20} color="currentColor" />
+            </div>
+            <span>Samundri - Faisalabad - Pakistan</span>
+          </div>
+          <div className="contact__section__left__link">
+            <div className="contact__section__left__link__icon">
+              <Mail size={20} color="currentColor" />
+            </div>
+            <span>mehfoozijaz786@gmail.com</span>
+          </div>
+          <div className="contact__section__left__link">
+            <div className="contact__section__left__link__icon">
+              <Phone size={20} color="currentColor" />
+            </div>
+            <span>+92 313 7178074</span>
+          </div>
+          <div className="contact__section__left__socials">
+            <a
+              href="https://github.com/MehfoozurRehman"
+              className="contact__section__left__socials__link"
+            >
+              <GitHub size={20} color="currentColor" />
+            </a>
+            <a
+              href="https://web.facebook.com/MehfoozurRehmanIjaz/"
+              className="contact__section__left__socials__link"
+            >
+              <Facebook size={20} color="currentColor" />
+            </a>
+            <a
+              href="https://www.instagram.com/mehfoozurrehman___/"
+              className="contact__section__left__socials__link"
+            >
+              <Instagram size={20} color="currentColor" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mehfooz-rehman-37a1b0231/"
+              className="contact__section__left__socials__link"
+            >
+              <Linkedin size={20} color="currentColor" />
+            </a>
+          </div>
         </div>
-        <div className="contact__section__left__socials">
-          <a
-            href="https://github.com/MehfoozurRehman"
-            className="contact__section__left__socials__link"
-          >
-            <GitHub size={20} color="currentColor" />
-          </a>
-          <a
-            href="https://web.facebook.com/MehfoozurRehmanIjaz/"
-            className="contact__section__left__socials__link"
-          >
-            <Facebook size={20} color="currentColor" />
-          </a>
-          <a
-            href="https://www.instagram.com/mehfoozurrehman___/"
-            className="contact__section__left__socials__link"
-          >
-            <Instagram size={20} color="currentColor" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mehfooz-rehman-37a1b0231/"
-            className="contact__section__left__socials__link"
-          >
-            <Linkedin size={20} color="currentColor" />
-          </a>
-        </div>
+        <form onSubmit={handleSubmit} className="contact__section__right">
+          <div className="contact__section__right__label">Name</div>
+          <input
+            type="text"
+            placeholder="Name"
+            className="contact__section__right__input__box"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            required
+          />
+          <div className="contact__section__right__label">Email Address</div>
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="contact__section__right__input__box"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+          <div className="contact__section__right__label">Subject</div>
+          <input
+            type="text"
+            placeholder="Subject"
+            className="contact__section__right__input__box"
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+            required
+          />
+          <div className="contact__section__right__label">Message</div>
+          <textarea
+            placeholder="Message"
+            rows="10"
+            className="contact__section__right__input__area"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            required
+          />
+          <button title="Send" className="contact__section__right__btn">
+            Send
+          </button>
+        </form>
       </div>
-      <form className="contact__section__right">
-        <div className="contact__section__right__label">Name</div>
-        <input
-          type="text"
-          placeholder="Name"
-          className="contact__section__right__input__box"
-        />
-        <div className="contact__section__right__label">Email Address</div>
-        <input
-          type="text"
-          placeholder="Email Address"
-          className="contact__section__right__input__box"
-        />
-        <div className="contact__section__right__label">Subject</div>
-        <input
-          type="text"
-          placeholder="Subject"
-          className="contact__section__right__input__box"
-        />
-        <div className="contact__section__right__label">Message</div>
-        <textarea
-          placeholder="Message"
-          rows="10"
-          className="contact__section__right__input__area"
-        />
-        <button title="Send" className="contact__section__right__btn">
-          Send
-        </button>
-      </form>
-    </div>
+    </>
   );
 }
