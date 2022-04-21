27a994c88f5aec89ba6996dacd7e7cf5c2b03c03
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-04-21 12:26:03 +0500

    about section done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./components/About";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <Header />
       <Home />
+      <About />
     </>
   );
 }
