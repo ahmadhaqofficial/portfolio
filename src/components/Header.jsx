c42be3ec0926e54da248a768f33e610b75a68d35
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-09 14:08:13 +0500

    Revert "header partially done"
    This reverts commit e52b097be964714a3b2325d39a9a75f86a2ea685.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { logo } from "../assets";
 import { Link } from "react-router-dom";
-import { Phone, Menu, X } from "react-feather";
 
 export default function Header() {
   return (
@@ -10,53 +9,7 @@ export default function Header() {
         <Link to="/" className="header__content__logo">
           <img src={logo} alt="logo" className="header__content__logo__img" />
         </Link>
-        <div className="header__content__nav">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/">Works</NavLink>
-          <NavLink to="/">Notes</NavLink>
-          <NavLink to="/">Contact</NavLink>
-        </div>
-        <div className="header__content__cta">
-          <a href="#" className="header__content__cta__link">
-            +92 313 7178074
-            <Phone size={20} color="currentColor" />
-          </a>
-          <button className="header__content__cta__link">
-            <Menu size={20} color="currentColor" />
-            <X size={20} color="currentColor" />
-          </button>
-        </div>
       </div>
     </div>
   );
-}
-
-function NavLink({ children, ...props }) {
-  return (
-    <div className="header__content__nav__link">
-      <input
-        type="radio"
-        title={children}
-        className="header__content__nav__link__input"
-        name="header__content__nav__link"
-        {...props}
-      />
-      <div className="header__content__nav__link__content">{children}</div>
-    </div>
-  );
-}
-
-{
-  /* <a href="#" className="header__content__cta__link">
-<GitHub size={20} color="currentColor" />
-</a>
-<a href="#" className="header__content__cta__link">
-<Facebook size={20} color="currentColor" />
-</a>
-<a href="#" className="header__content__cta__link">
-<Instagram size={20} color="currentColor" />
-</a>
-<a href="#" className="header__content__cta__link">
-<Linkedin size={20} color="currentColor" />
-</a> */
 }
