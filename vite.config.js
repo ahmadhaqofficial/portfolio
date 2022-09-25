7dae409a68fe84a75f0641f987bc852fc923b56c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-25 23:51:54 +0500

    changes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,64 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { imagetools } from "vite-imagetools";
+import viteImagemin from "vite-plugin-imagemin";
+import viteCompression from "vite-plugin-compression";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      "process.env.NODE_ENV": `"${mode}"`,
-    },
-  });
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    ViteWebfontDownload(),
+    chunkSplitPlugin(),
+    imagetools(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      threshold: 100,
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.7, 0.8],
+        speed: 4,
+      },
+      webp: {
+        quality: 70,
+      },
+      svgo: {
+        multipass: true,
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "minifyStyles",
+          },
+          {
+            name: "removeMetadata",
+          },
+          {
+            name: "removeUselessStrokeAndFill",
+          },
+          {
+            name: "reusePaths",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: true,
+          },
+        ],
+      },
+    }),
+  ],
+});
